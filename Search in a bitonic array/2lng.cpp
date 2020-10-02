5b2cb241d753c48053bb59f535ab667a08df5ed7
Ray08charles <45870004+red574890@users.noreply.github.com>
2020-10-02 16:59:28 +0800

    Update 2lng.cpp
    In this algorithm, we don't search the peak first. Instead, we search the peak when current mid is not the peak. However, we still need to search both side of the mid. Therefore, we can process the data through compare ~2lnN times.

--- Search in a bitonic array/2lng.cpp
+++ Search in a bitonic array/2lng.cpp
@@ -8,45 +8,94 @@
 
 #include <iostream>
 using namespace std;
+
+int rightsearch(int high,int low,int bit[],int val );
+int leftsearch(int high,int low, int bit[],int val );
+
 int main(int argc, const char * argv[]) {
     //create a bitonic array
-    int bit[10]={0,1,2,10,9,8,7,6,5,4};
+    int bit[10]={0,12,11,10,9,8,7,6,5,4};
     //find the index of biggest value
-    int mid=(sizeof(bit)/sizeof(bit[0]))/2;
+    int mid;
     int low=0;
     int high=(sizeof(bit)/sizeof(bit[0]))-1;
-    int val=10;
+    int val=12;
+    int f1;
+    int f2;
+    int res=-1;
+    low=0;
+    
+    int stop=-1;
+
+    
+
+    
+    
+    
+    while (low<=high && stop==-1) {
+        mid=low+((high-low)/2);
+        if(val<=bit[mid]){
+            f1=rightsearch(high, mid, bit, val);
+            f2=leftsearch(mid, low, bit, val);
+            if(f1!=-1){res=f1;}
+            if(f2!=-1){res=f2;}
+            stop=1;
+            
+        }else{
+            if(bit[mid]<bit[mid+1]){  // if the current mid is not mid, search the peak 
+                low=mid;
+            }else{
+                high=mid;
+            }
+        }
+    }
+    cout<<res;
+
+    
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+int leftsearch(int high,int low,int bit[],int val ){ // search the left side
     int f=-1;
-    
-    low=mid;
-    
-    while (low<high) {
-        mid=(high+low)/2;
+    int mid;
+        while (low<=high) {
+        mid=low+(high-low)/2;
+        if(val==bit[mid]){
+            f=mid;
+            break;
+        }else if(val<bit[mid]){
+            high=mid-1;
+        }else{
+            low=mid+1;
+        }
+    }
+    return f;
+}
+
+int rightsearch(int high,int low, int bit[],int val ){   //search the right side
+    int f=-1;
+    int mid;
+    while (low<=high) {
+        mid=low+(high-low)/2;
         if(val==bit[mid]){
             f=mid;
             break;
         }else if(val<bit[mid]){
             low=mid+1;
         }else{
-            high=mid;
+            high=mid-1;
         }
     }
-
-    
-    mid=(sizeof(bit)/sizeof(bit[0]))/2;
-    low=0;
-    high=mid-1;
-    
-    while (low<high-1) {
-        mid=(high+low)/2;
-        if(val==bit[mid]){
-            f=mid;
-            break;
-        }else if(val<bit[mid]){
-            high=mid;
-        }else{
-            low=mid;
-        }
-    }
-    
-    cout<<f;
+    return f;
+}
